3c7533f8a02975a0f0471c502faf197293a821e9
Jeryong Ryu <wima90@gmail.com>
2020-12-17 01:23:27 +0900

    응~ 하나도 모르겠다~

--- models/intent/train_model.py
+++ models/intent/train_model.py
@@ -86,7 +86,7 @@ concat = concatenate([pool1, pool2, pool3])
 hidden = Dense(128, activation=tf.nn.relu)(concat)
 dropout_hidden = Dropout(rate=dropout_prob)(hidden)
 logits = Dense(5, name='logits')(dropout_hidden)
-predictions = Dense(5, activation=tf.nn.softmax)(logits)
+predictions = Dense(30, activation=tf.nn.softmax)(logits)
 
 
 # 모델 생성  ○5
